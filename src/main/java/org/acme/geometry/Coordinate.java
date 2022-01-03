276a7f54a81283a32dc7642cc585682671c21b79
Axelle-Ga <axelle.gaige@gmail.com>
2022-01-03 13:48:46 +0100

    0.1 Feat(coordinate): implement coordinate 2D

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -2,4 +2,24 @@ package org.acme.geometry;
 
 public class Coordinate {
 
+    private double x;
+    private double y;
+
+    Coordinate(){
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    Coordinate(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
